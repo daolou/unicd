3f1d946deab09c8ed4e5329159cf160a9a369649
叨喽 <zhiguo.jzg@alibaba-inc.com>
2021-03-30 21:35:22 +0800

    chore: 🔧 optimize publish.sh

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -1,12 +1,43 @@
 #!/usr/bin/env bash
 
-npm config get registry # 检查仓库镜像库
-npm config set registry=http://registry.npmjs.org
+npmjs=http://registry.npmjs.org
+origin=$(npm config get registry) # 原仓库镜像
+
+echo "原仓库镜像为 $origin"
+printf '\t\n'
+
+if [[ "$npmjs" != "$origin" ]]; then
+  npm config set registry="$npmjs"
+  echo "已将仓库镜像设置为 $npmjs"
+  printf '\t\n'
+fi
+
+echo "-------开始编译-------"
+printf '\t\n'
 npm run compile
-echo '请进行登录相关操作：'
-npm login # 登陆
-echo "-------publishing-------"
-npm publish # 发布
-npm config set registry=https://registry.npm.taobao.org # 设置为淘宝镜像
-echo "发布完成"
-exit
+echo "-------结束编译-------"
+printf '\t\n'
+
+# 是否已经登陆
+current=$(npm whoami 2>/dev/null)
+if [[ $(echo $?) == 1 ]]; then
+  echo '请先进行登录相关操作：npm login'
+  exit 0
+else
+  echo "当前已登陆：$current"
+  printf '\t\n'
+fi
+
+echo "-------开始发布-------"
+printf '\t\n'
+npm publish
+echo "-------发布完成-------"
+printf '\t\n'
+
+if [[ "$npmjs" != "$origin" ]]; then
+  # 设置为原仓库镜像
+  npm config set registry="$origin"
+  echo "已还原仓库镜像为 $origin"
+fi
+
+exit 0
